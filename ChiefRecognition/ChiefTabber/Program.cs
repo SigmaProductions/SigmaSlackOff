543e7898fa73005f5d5ba2857affb87c6a6d9a82
MacenDi <mstanekofficial@gmail.com>
2019-11-22 22:32:40 +0100

    ChiefTabber added

---
+++ ChiefRecognition/ChiefTabber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ChiefTabber
+{
+    class Program
+    {
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg, IntPtr wParam, IntPtr lParam);
+
+        const int WM_COMMAND = 0x111;
+        const int MIN_ALL = 419;
+
+        static void Main(string[] args)
+        {
+            IntPtr lHwnd = FindWindow("Shell_TrayWnd", null);
+            SendMessage(lHwnd, WM_COMMAND, (IntPtr)MIN_ALL, IntPtr.Zero);
+            Thread.Sleep(1000);
+        }
+    }
+}
