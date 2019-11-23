e883add6fb80e1141bfcfd662bb348a1b2a6101f
MacenDi <mstanekofficial@gmail.com>
2019-11-23 02:54:04 +0100

    Changer ui added

--- ChiefRecognition/ChiefRecognition/Program.cs
+++ ChiefRecognition/ChiefRecognition/Program.cs
@@ -11,19 +11,20 @@ namespace ChiefRecognition
     {
         static void Main(string[] args)
         {
-            var comparer = new FaceComparer();
-            comparer.sourceImage = "girlresized.jpg";
-            comparer.targetImage = "girlsresized.jpg";
-            Task.Run(async () =>
-            {
-                await comparer.IsSimillarFaceOnSourceImage();
-            }).GetAwaiter().GetResult();
+            //var comparer = new FaceComparer();
+            //comparer.sourceImage = "girlresized.jpg";
+            //comparer.targetImage = "girlsresized.jpg";
+            //Task.Run(async () =>
+            //{
+            //    await comparer.IsSimillarFaceOnSourceImage();
+            //}).GetAwaiter().GetResult();
 
             var connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:53353/ChatHub")
+                .WithUrl("https://localhost:44354/homehub")
                 .Build();
 
-            
+            Task.Run(async () => { await connection.StartAsync(); }).GetAwaiter().GetResult();
+            connection.SendAsync("HideWindows");
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
