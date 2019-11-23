3da02baf367c7e4f3c369aac5e270ed2c1cb15e0
unknown <sendek.bartosz@gmail.com>
2019-11-23 09:40:26 +0100

    lmaao

--- WebFront/src/app/lobbies/lobbies.component.ts
+++ WebFront/src/app/lobbies/lobbies.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Room } from '../models/room.model';
 import { DataService } from './data.service';
 import {Router} from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { LoginComponent } from '../login/login.component';
 import { User } from '../login/user';
+import { DxListComponent } from 'devextreme-angular';
 
 @Component({
   selector: 'app-lobbies',
@@ -13,17 +14,18 @@ import { User } from '../login/user';
 })
 export class LobbiesComponent implements OnInit {
   data: Array<Room>;
-  selectedItem: Room;
+  selectedItems: Room;
 
   constructor(private dataService: DataService, private router:Router, private httpClient: HttpClient) { 
     this.dataService.getData().toPromise().then((val)=>{this.data= val});
   }
   joinClick(e){ 
     this.httpClient.put("https://localhost:5001/api/Room/assignUser",
-        {roomId: this.selectedItem.id, userId : User.Username});
+        {roomId: this.selectedItems.id, userId : User.Username});
     this.router.navigateByUrl('/cool');
   }
   ngOnInit() {
+    this.selectedItems = new Room();
   }
 
 }
