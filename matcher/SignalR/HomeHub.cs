5ddb9e78d45583f0e973702bdb9dbd94e9772c1b
MacenDi <mstanekofficial@gmail.com>
2019-11-23 02:18:37 +0100

    HomehubAdded

---
+++ matcher/SignalR/HomeHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace matcher.SignalR
+{
+    public class HomeHub : Hub
+    {
+        [HubMethodName("HideWindows")]
+         public async Task HideAllWindows()
+        {
+            await Clients.All.SendAsync("HideWindows");
+        }
+    }
+}
